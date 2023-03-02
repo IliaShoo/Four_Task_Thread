b1de89706a75aec68dfc19b64418f1cfd518b49c
Ilia Shudegov <faun-08@mail.ru>
2023-03-02 23:24:32 +0300

    searchMethodAdd

--- src/Main.java
+++ src/Main.java
@@ -1,105 +1,51 @@
 import java.util.Random;
 import java.util.concurrent.ArrayBlockingQueue;
 import java.util.concurrent.BlockingQueue;
-import java.util.concurrent.atomic.AtomicInteger;
 
 public class Main {
 
-    public static BlockingQueue A_AMOUNT = new ArrayBlockingQueue<>(100);
-    public static BlockingQueue B_AMOUNT = new ArrayBlockingQueue<>(100);
-    public static BlockingQueue C_AMOUNT = new ArrayBlockingQueue<>(100);
-
-    static AtomicInteger A = new AtomicInteger();
-    static AtomicInteger B = new AtomicInteger();
-    static AtomicInteger C = new AtomicInteger();
+    public static BlockingQueue<String> A_AMOUNT = new ArrayBlockingQueue<>(100);
+    public static BlockingQueue<String> B_AMOUNT = new ArrayBlockingQueue<>(100);
+    public static BlockingQueue<String> C_AMOUNT = new ArrayBlockingQueue<>(100);
 
     public static void main(String[] args) throws InterruptedException {
 
-        Random random = new Random();
-        String[] texts = new String[10_000];
-        for (int i = 0; i < texts.length; i++) {
-            texts[i] = generateText("abc", 100_000);
-        }
-
-        Runnable generateWords = () -> {
-
+        Thread cloneThread = new Thread(() -> {
+            for (int i = 0; i < 10_000; i++) {
+                String word = generateText("abc", 100_000);
                 try {
-                    for (int i = 0; i < 10_000; i++) {
-                        A_AMOUNT.put(texts[i]);
-                        B_AMOUNT.put(texts[i]);
-                        C_AMOUNT.put(texts[i]);
-                    }
+                    A_AMOUNT.put(word);
+                    B_AMOUNT.put(word);
+                    C_AMOUNT.put(word);
                 } catch (InterruptedException e) {
-                    return;
+                    throw new RuntimeException(e);
                 }
-        };
+            }
+        });
 
-        Thread cloneThread = new Thread(generateWords);
         cloneThread.start();
 
         Runnable aExamination = () -> {
-            for (int i = 0; i < 10_000; i++) {
-                String word = String.valueOf(A_AMOUNT.element());
-                int letterCounter = (int) word.chars().filter(ch -> ch == 'a').count();
-                if (letterCounter > A.get()) {
-                    A.getAndSet(letterCounter);
-                } else {
-                    try {
-                        A_AMOUNT.take();
-                    } catch (InterruptedException e) {
-                        return;
-                    }
-                }
-            }
+            char letter = 'a';
+            int maxA = maxChar(A_AMOUNT, letter);
+            System.out.println("Самое большое количество повторений \"а\" " + maxA);
         };
         Thread aThread = new Thread(aExamination);
         aThread.start();
 
         Runnable bExamination = () -> {
-            for (int i = 0; i < 10_000; i++) {
-                String word = String.valueOf(B_AMOUNT.element());
-                int letterCounter = (int) word.chars().filter(ch -> ch == 'b').count();
-                if (letterCounter > B.get()) {
-                    B.getAndSet(letterCounter);
-                    B_AMOUNT.clear();
-                    try {
-                        B_AMOUNT.put(word);
-                    } catch (InterruptedException e) {
-                        return;
-                    }
-                } else {
-                    try {
-                        B_AMOUNT.take();
-                    } catch (InterruptedException e) {
-                        return;
-                    }
-                }
-            }
+            char letter = 'b';
+            int maxA = maxChar(B_AMOUNT, letter);
+            System.out.println("Самое большое количество повторений \"b\" " + maxA);
         };
 
         Thread bThread = new Thread(bExamination);
         bThread.start();
 
         Runnable cExamination = () -> {
-            for (int i = 0; i < 10_000; i++) {
-                String word = String.valueOf(C_AMOUNT.element());
-                int letterCounter = (int) word.chars().filter(ch -> ch == 'c').count();
-                if (letterCounter > C.get()) {
-                    C.getAndSet(letterCounter);
-                    C_AMOUNT.clear();
-                    try {
-                        C_AMOUNT.put(word);
-                    } catch (InterruptedException e) {
-                        return;
-                    }
-                } else {
-                    try {
-                        C_AMOUNT.take();
-                    } catch (InterruptedException e) {
-                        return;
-                    }
-                }
-            }
+            char letter = 'c';
+            int maxA = maxChar(C_AMOUNT, letter);
+            System.out.println("Самое большое количество повторений \"c\" " + maxA);
         };
 
         Thread cTread = new Thread(cExamination);
@@ -109,9 +55,6 @@ public class Main {
         bThread.join();
         cTread.join();
 
-        System.out.println("Самое длинное слово с буквой \"а\" " + A + "\n" +
-                "Самое длинное слово с буквой \"b\" " + B + "\n" +
-                "Самое длинное слово с буквой \"c\" " + C + "\n");
         cloneThread.interrupt();
 
     }
@@ -124,4 +67,24 @@ public class Main {
         }
         return text.toString();
     }
+
+    public static int maxChar(BlockingQueue<String> blockingQueue, char ch) {
+        int count = 0;
+        int max = 0;
+        String word;
+        for (int i = 0; i < 10_000; i++) {
+            try {
+                word = blockingQueue.take();
+                for (char c : word.toCharArray()) {
+                    if (c == ch) count++;
+                }
+                if (count > max) max = count;
+                count = 0;
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+        }
+        return max;
+    }
+
 }
